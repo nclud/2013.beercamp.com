929d59648a819c9a89ea6fab06027c7bc165c5f0
mikemorris <mike@nclud.com>
2013-02-20 19:58:18 -0500

    updating haibu deploy script

--- deploy.js
+++ deploy.js
@@ -9,18 +9,28 @@ var client = new haibu.drone.Client({
 
 // A basic package.json for a node.js application on Haibu
 var app = {
-   "user": "mikemorris",
-   "name": "beercamp",
-   "repository": {
-     "type": "git",
-     "url": "https://github.com/mikemorris/beercamp.git"
-   },
-   "scripts": {
-     "start": "server.js"
-   },
-   "engine": {
-     "node": "0.8.x"
-   }
+  "user": "mikemorris",
+  "name": "beercamp",
+  "repository": {
+   "type": "git",
+   "url": "https://github.com/mikemorris/beercamp.git"
+  },
+  "scripts": {
+   "start": "server.js"
+  },
+  "engine": {
+   "node": "0.8.x"
+  },
+  "dependencies": {
+    "socket.io": "0.9.x",
+    "node-static": "0.6.x",
+    "underscore": "1.4.x",
+    "async": "0.1.x",
+    "redis": "0.8.x",
+    "node-uuid": "1.4.x",
+    "box2dweb-commonjs": "2.1.x"
+  },
+  "analyze": true
 };
 
 // Attempt to start up a new application
