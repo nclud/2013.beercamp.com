bc154911d0eb36bb41d89173b254ae496dc15bb0
mikemorris <mike@nclud.com>
2013-03-05 16:57:45 -0500

    rewiring ui.init()

--- game/client/init.js
+++ game/client/init.js
@@ -1,13 +1,19 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD
-    define(['../core/core', 'input', 'client'], factory);
+    define([
+      '../core/core',
+      './input',
+      './ui',
+      './client'
+    ], factory);
   }
-})(this, function(core, input, client) {
+})(this, function(core, input, ui, client) {
 
   // game.debug = true;
   core.initGlobalVariables();
   input.init();
+  ui.init(client);
   client.createCanvas();
   client.init(client);
   client.play();
