f553f799a6670adb549f108276ba866929fda838
jared laser <laserjn@gmail.com>
2013-03-08 13:44:00 -0500

    fuck it, we'll do it live!

--- public/config.rb
+++ public/config.rb
@@ -8,16 +8,20 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+sass_options = {
+    :debug_info => true
+}
