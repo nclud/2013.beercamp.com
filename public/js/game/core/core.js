aede89bc2cd0f6f634e5d82441edbbcffed7026a
Patrick Peak <peakpg@gmail.com>
2013-03-12 09:54:01 -0400

    [#70] Optimize full state
    Full state update for a single player is now ~400b.
    
    * Remove unnecessary sprite data from serialization.
    * X/Y and velocities should be stored as lower precision. (Shrinks message size from server)
    * Shorten up all the attributes for full state updates (i.e. velocity to v)

--- public/js/game/core/core.js
+++ public/js/game/core/core.js
@@ -23,6 +23,23 @@
   };
   */
 
+  // Used to prevent sending overly precise numbers from server to client.
+  // Integers should not be given additional precision
+  var toFixed = function(number){
+    if(typeof number.toFixed !== 'function'){
+      number = parseFloat(number);
+    }
+    if(this.isInt(number)){
+      return number;
+    }
+    return number.toFixed(2);
+
+
+  };
+
+  var isInt = function(number) {
+     return number % 1 === 0;
+  };
   // linear interpolation
   var lerp = function(prev, next, time) {
     var _prev = Number(prev);
@@ -141,7 +158,9 @@
     initGlobalVariables: initGlobalVariables,
     isCollision: isCollision,
     loadScene: loadScene,
-    filterQueue: filterQueue
+    filterQueue: filterQueue,
+    toFixed:toFixed,
+    isInt:isInt
   };
 
 });
