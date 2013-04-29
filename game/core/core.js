8ae10cd5e04ef28ec81b34e6867d14fab4fc54bb
jared laser <laserjn@gmail.com>
2013-04-29 14:27:20 -0400

    cleaning up old trash code

--- game/core/core.js
+++ game/core/core.js
@@ -8,21 +8,6 @@
   }
 })(this, function() {
 
-  // Array cleaner removes specfied value from array. In this case,
-  // I'm using it to remove 'undefined' objects in the array.
-  // http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
-  /*
-  Array.prototype.clean = function(deleteValue) {
-    for (var i = 0; i < this.length; i++) {
-      if (this[i] == deleteValue) {
-        this.splice(i, 1);
-        i--;
-      }
-    }
-    return this;
-  };
-  */
-
   // Used to prevent sending overly precise numbers from server to client.
   // Integers should not be given additional precision
   var toFixed = function(number) {
